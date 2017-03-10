05f1ead2319e3b3f45b4ee3af329a970b19982f7
Enrico <eddmler@gmail.com>
2017-03-10 19:00:07 +0100

    Add pickup/putdown constraints

--- test.php
+++ test.php
@@ -9,23 +9,23 @@ echo "\n";
 $dir = scandir(__DIR__ . '/test');
 
 foreach ($dir as $file) {
-	if (substr($file, -3, 3) == ".lp") {
-		$result_file = file_get_contents(__DIR__ . '/test/result/' . $file . '.txt');
-		$results = explode(' ', $result_file);
+    if (substr($file, -3, 3) == ".lp") {
+        $result_file = file_get_contents(__DIR__ . '/test/result/' . $file . '.txt');
+        $results = explode(' ', $result_file);
 
-		$clingo_all = shell_exec($clingo_path . ' rob.lp test/' . $file . ' 0');
+        $clingo_all = shell_exec($clingo_path . ' rob.lp test/' . $file . ' 0');
 
-		$clingo = explode('Answer:', $clingo_all);
-		$clingo = array_pop($clingo);
+        $clingo = explode('Answer:', $clingo_all);
+        $clingo = array_pop($clingo);
 
-		foreach ($results as $result) {
-			if (stripos($clingo, $result) === false) {
-				echo shell_exec($clingo_path . ' rob.lp test/' . $file . ' 0');
-				echo "\n\n\n\033[31mFEHLER: Test " . $file . " ist fehlgeschlagen. \033[0mKonnte nicht finden:\n" . $result . "\n\nOben ist die Ausgabe von Clingo für diesen Test.\n";
-				exit;
-			}
-		}
-	}
+        foreach ($results as $result) {
+            if (stripos($clingo, $result) === false) {
+                echo shell_exec($clingo_path . ' rob.lp test/' . $file . ' 0');
+                echo "\n\n\n\033[31mFEHLER: Test " . $file . " ist fehlgeschlagen. \033[0mKonnte nicht finden:\n" . $result . "\n\nOben ist die Ausgabe von Clingo für diesen Test.\n";
+                exit;
+            }
+        }
+    }
 }
 
 echo "\033[32mEverything OK!\n";
